a50b2bb5f6820b4f1459a712671ce16c0cb7ba0c
Jack Fogarty <jackfogarty2105@gmail.com>
2021-02-28 13:34:37 +0800

    SQL Setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Vegan_Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Quarter_Pounder", false);
